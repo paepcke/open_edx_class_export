47485bce9cba39f6f6acc3f1435bae7fc0d690cb
Andreas Paepcke <paepcke@cs.stanford.edu>
2014-04-11 17:30:32 -0700

    Bumped required pymysql_utils version to 0.27. Put some debugging into start_export_class.sh; that needs to be revised

--- scripts/start_export_class.sh
+++ scripts/start_export_class.sh
@@ -11,10 +11,13 @@ currScriptsDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $currScriptsDir/..
 
-export PYTHONPATH=/home/paepcke/.virtualenvs/online_learning_computations/lib/python2.7/site-packages/online_learning_computations-0.26-py2.7.egg/src:$PYTHONPATH
+startupCmd="export PYTHONPATH=$WORKON_HOME/online_learning_computations/lib/python2.7/site-packages/online_learning_computations-0.26-py2.7.egg/src:$PYTHONPATH \
+export PYTHONPATH=$WORKON_HOME/online_learning_computations/lib/python2.7/site-packages/online_learning_computations-0.26-py2.7.egg:$PYTHONPATH \
+export PYTHONPATH=$WORKON_HOME/open_edx_class_export/local/lib/python2.7/site-packages/pymysql_utils-0.25-py2.7.egg:$PYTHONPATH \
+/usr/bin/python src/exportClass.py"
 
-export PYTHONPATH=/home/paepcke/.virtualenvs/online_learning_computations/lib/python2.7/site-packages/online_learning_computations-0.26-py2.7.egg:$PYTHONPATH
-
-export PYTHONPATH=/lfs/datastage/0/home/paepcke/.virtualenvs/open_edx_class_export/local/lib/python2.7/site-packages/pymysql_utils-0.25-py2.7.egg:$PYTHONPATH
-
-/usr/bin/python src/exportClass.py
+# This script is called as root.
+# Drop root permissions to whose of the owner
+# of this script:
+SCRIPT_OWNER=`stat -c %U  $0`
+su -m -c $startupCmd  $SCRIPT_OWNER
