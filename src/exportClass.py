b3c0ad89d42bc913bfabf6266b0547e31010ea37
Alex Kindel <atkindel@stanford.edu>
2015-06-12 11:38:37 -0700

    Debugging survey getter.

--- src/exportClass.py
+++ src/exportClass.py
@@ -1342,15 +1342,15 @@ class DataServer(threading.Thread):
         # Export data for each survey ID (usually not more than 2 surveys)
         for idx, surveyID in enumerate(svIDs):
             surveyOutfile = os.path.join(self.fullTargetDir, '%s_survey%d.csv' % (courseNameNoSpaces, idx+1))
-            surveyQuery = dbQuery.substitute(filename=surveyOutfile, table="Survey", svID=surveyID)
+            surveyQuery = dbQuery.substitute(filename=surveyOutfile, table="Survey", svID=surveyID[0])
             self.mysqlDb.query(surveyQuery).next()
 
             answerOutfile = os.path.join(self.fullTargetDir, '%s_survey%d_answer.csv' % (courseNameNoSpaces, idx+1))
-            answerQuery = dbQuery.substitute(filename=answerOutfile, table="Answer", svID=surveyID)
+            answerQuery = dbQuery.substitute(filename=answerOutfile, table="Answer", svID=surveyID[0])
             self.mysqlDb.query(answerQuery).next()
 
             answermetaOutfile = os.path.join(self.fullTargetDir, '%s_survey%d_answermeta.csv' % (courseNameNoSpaces, idx+1))
-            answermetaQuery = dbQuery.substitute(filename=answermetaOutfile, table="AnswerMeta", svID=surveyID)
+            answermetaQuery = dbQuery.substitute(filename=answermetaOutfile, table="AnswerMeta", svID=surveyID[0])
             self.mysqlDb.query(answermetaQuery).next()
 
         # Save information for printTableInfo() method to find:
