631d1c4ad2a5dd37595a824537a9c61f94adc2fc
Alex Kindel <atkindel@stanford.edu>
2015-06-12 14:01:04 -0700

    Debugging survey getter.

--- src/exportClass.py
+++ src/exportClass.py
@@ -1329,7 +1329,7 @@ class DataServer(threading.Thread):
         # Get list of survey IDs
         idgetter = "SELECT SurveyId FROM EdxQualtrics.SurveyInfo WHERE course_display_name = '%s'" % courseId
         svIter = self.mysqlDb.query(idgetter)
-        svIDs = list(svIter)
+        svIDs = svID[0] for svID in svIter
 
         # Define query template
         dbQuery = Template( """
