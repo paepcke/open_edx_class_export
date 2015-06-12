c3dacfb76331041c9c61772db1c0f8cbf598aa8b
Alex Kindel <atkindel@stanford.edu>
2015-06-12 10:10:16 -0700

    Testing message passing.

--- src/exportClass.py
+++ src/exportClass.py
@@ -1322,7 +1322,7 @@ class DataServer(threading.Thread):
         :param detailDict: dict of arguments; expected: 'courseId', 'wipeExisting'
         :type detailDict: {String : String, String : Boolean}
         '''
-        courseID = detailDict.get('courseId', '').strip()
+        courseID = detailDict['courseId'].strip()
         courseNameNoSpaces = string.replace(string.replace(courseId,' ',''), '/', '_')
         surveyOutfile = os.path.join(self.fullTargetDir, '%s_survey.csv' % (courseNameNoSpaces))
 
