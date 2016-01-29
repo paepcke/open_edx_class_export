a40575d5a02a851c3d5dc77230bed080952e6c7b
Alex Kindel <atkindel@stanford.edu>
2016-01-29 11:42:13 -0800

    Fix echo commmand opts

--- scripts/makeForumCSV.sh
+++ scripts/makeForumCSV.sh
@@ -470,7 +470,7 @@ echo "Creating Forum extract ...<br>"
 set -o pipefail
 # Exit on error:
 set -e
-echo -E "$EXPORT_FORUM_CMD" | mysql $MYSQL_AUTH
+echo -E "$EXPORT_FORUM_CMD" | mysql -r $MYSQL_AUTH
 
 # Concatenate the col name header and the table,
 #
